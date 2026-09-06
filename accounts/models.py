edc0cdd356c08b12fcbd86c825b3cabfbddfe92c
azroku <akuric1@etf.unsa.ba>
2026-09-06 20:59:02 +0200

    Add files via upload

---
+++ accounts/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        STUDENT = "student", "Student"
+        PROFESSOR = "professor", "Professor"
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.STUDENT,
+    )
+
+    def __str__(self):
+        return f"{self.username} ({self.get_role_display()})"
