2c967ffd2641a7738fc5eb5679018bae53611a99
azroku <akuric1@etf.unsa.ba>
2026-09-06 23:33:45 +0200

    Update admin.py

--- courses/admin.py
+++ courses/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import Course, Enrollment
-
+from .models import Course, Enrollment, KnowledgeComponent, StudentKnowledgeState, LearningAttempt, LearningSession
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
@@ -38,3 +37,129 @@ class EnrollmentAdmin(admin.ModelAdmin):
         "student__email",
     )
     readonly_fields = ("enrolled_at",)
+
+@admin.register(KnowledgeComponent)
+class KnowledgeComponentAdmin(admin.ModelAdmin):
+    list_display = (
+        "external_id",
+        "name",
+        "course",
+        "parent",
+    )
+    list_filter = ("course",)
+    search_fields = (
+        "external_id",
+        "name",
+        "course__code",
+        "course__name",
+    )
+
+@admin.register(StudentKnowledgeState)
+class StudentKnowledgeStateAdmin(admin.ModelAdmin):
+    list_display = (
+        "student_display",
+        "component_display",
+        "course_display",
+        "mastery_prob",
+        "last_attempt_at",
+    )
+    list_filter = (
+        "enrollment__course",
+        "knowledge_component__course",
+    )
+    search_fields = (
+        "enrollment__student__username",
+        "enrollment__student__email",
+        "knowledge_component__external_id",
+        "knowledge_component__name",
+        "enrollment__course__code",
+        "enrollment__course__name",
+    )
+    raw_id_fields = (
+        "enrollment",
+        "knowledge_component",
+    )
+
+    def student_display(self, obj):
+        return obj.enrollment.student.username
+
+    def component_display(self, obj):
+        return obj.knowledge_component.external_id
+
+    def course_display(self, obj):
+        return obj.enrollment.course.code
+
+    student_display.short_description = "Student"
+    component_display.short_description = "Component"
+    course_display.short_description = "Course"
+
+@admin.register(LearningAttempt)
+class LearningAttemptAdmin(admin.ModelAdmin):
+    list_display = (
+        "student_display",
+        "component_display",
+        "course_display",
+        "is_correct",
+        "attempted_at",
+    )
+    list_filter = (
+        "is_correct",
+        "enrollment__course",
+        "knowledge_component__course",
+    )
+    search_fields = (
+        "enrollment__student__username",
+        "enrollment__student__email",
+        "knowledge_component__external_id",
+        "knowledge_component__name",
+        "enrollment__course__code",
+        "enrollment__course__name",
+    )
+    raw_id_fields = (
+        "enrollment",
+        "knowledge_component",
+    )
+    date_hierarchy = "attempted_at"
+
+    def student_display(self, obj):
+        return obj.enrollment.student.username
+
+    def component_display(self, obj):
+        return obj.knowledge_component.external_id
+
+    def course_display(self, obj):
+        return obj.enrollment.course.code
+
+    student_display.short_description = "Student"
+    component_display.short_description = "Component"
+    course_display.short_description = "Course"
+
+@admin.register(LearningSession)
+class LearningSessionAdmin(admin.ModelAdmin):
+    list_display = (
+        "student_display",
+        "course_display",
+        "started_at",
+        "ended_at",
+        "duration_seconds",
+    )
+    list_filter = (
+        "enrollment__course",
+    )
+    search_fields = (
+        "enrollment__student__username",
+        "enrollment__student__email",
+        "enrollment__course__code",
+        "enrollment__course__name",
+    )
+    raw_id_fields = ("enrollment",)
+    date_hierarchy = "started_at"
+
+    def student_display(self, obj):
+        return obj.enrollment.student.username
+
+    def course_display(self, obj):
+        return obj.enrollment.course.code
+
+    student_display.short_description = "Student"
+    course_display.short_description = "Course"
